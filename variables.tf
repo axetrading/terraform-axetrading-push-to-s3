5fcf1676a7d5e492ecea2b110eb59e4b1b7fea6e
Ciprian <cursu@axetrading.com>
2022-09-09 16:33:16 +0300

    revert changes

--- variables.tf
+++ variables.tf
@@ -29,7 +29,7 @@ variable "existing_s3_bucket" {
 }
 
 variable "objects_to_push" {
-  type        = any
+  type        = map(any)
   default     = {}
   description = "A list of terraform objects that will be encoded to yaml and pushed to S3"
 }
