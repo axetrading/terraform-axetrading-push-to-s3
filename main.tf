7bc813df9ed67af5fbb3739f40102a1b8e1e21a9
Ciprian <cursu@axetrading.com>
2022-09-09 14:37:11 +0300

    adding an additional s3 object - it will be used to store the yaml encoded terraform object

--- main.tf
+++ main.tf
@@ -1,35 +1,42 @@
 locals {
-  files = toset(flatten([for relative_path in var.config_paths : fileset(path.root, "${relative_path}/*.yaml")]))
+  existing_s3_bucket = var.existing_s3_bucket != null && var.existing_s3_bucket != ""
+  files              = var.push_files_to_s3 ? toset(flatten([for relative_path in var.config_paths : fileset(path.root, "${relative_path}/*.yaml")])) : []
+  objects            = var.push_objects_to_s3 ? { for k, v in var.objects_to_push : k => v if var.objects_to_push != null && var.objects_to_push != {} } : {}
 }
 
-resource "aws_s3_bucket" "resource_configs" {
+resource "aws_s3_bucket" "main" {
+  count  = local.existing_s3_bucket ? 0 : 1
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_public_access_block" "resource_configs" {
-  bucket                  = aws_s3_bucket.resource_configs.id
+resource "aws_s3_bucket_public_access_block" "main" {
+  count                   = local.existing_s3_bucket ? 0 : 1
+  bucket                  = aws_s3_bucket.main[0].id
   block_public_acls       = true
   block_public_policy     = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_versioning" "resource_configs" {
-  bucket = aws_s3_bucket.resource_configs.id
+resource "aws_s3_bucket_versioning" "main" {
+  count  = local.existing_s3_bucket ? 0 : 1
+  bucket = aws_s3_bucket.main[0].id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "resource_configs" {
-  bucket = aws_s3_bucket.resource_configs.id
+resource "aws_s3_bucket_ownership_controls" "main" {
+  count  = local.existing_s3_bucket ? 0 : 1
+  bucket = aws_s3_bucket.main[0].id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "resource_configs" {
-  bucket = aws_s3_bucket.resource_configs.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  count  = local.existing_s3_bucket ? 0 : 1
+  bucket = aws_s3_bucket.main[0].bucket
 
   rule {
     apply_server_side_encryption_by_default {
@@ -38,14 +45,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "resource_configs"
   }
 }
 
-resource "aws_s3_object" "config_files" {
+resource "aws_s3_object" "files" {
   for_each = local.files
 
-  bucket = aws_s3_bucket.resource_configs.id
-  key    = each.value
-  source = each.value
+  bucket       = local.existing_s3_bucket ? var.existing_s3_bucket : aws_s3_bucket.main[0].id
+  key          = each.value
+  source       = each.value
   content_type = "text/yaml"
-  etag   = filemd5(each.value)
+  etag         = filemd5(each.value)
 }
 
+resource "aws_s3_object" "outputs" {
+  for_each = local.objects
 
+  bucket       = local.existing_s3_bucket ? var.existing_s3_bucket : aws_s3_bucket.main[0].id
+  key          = format("%s-%s-%s", "terraform", each.key, ".yaml" )
+  content      = replace(yamlencode(each.value), "\"", "")
+  content_type = "text/yaml"
+}
--- variables.tf
+++ variables.tf
@@ -1,9 +1,34 @@
 variable "config_paths" {
   description = "Path or paths for the configuration files. "
   type        = set(string)
+  default     = []
 }
 
 variable "bucket_name" {
   type        = string
   description = "AWS S3 Bucket name"
+}
+
+variable "push_files_to_s3" {
+  type        = bool
+  description = "Push files to S3 bucket"
+  default     = false
+}
+
+variable "push_objects_to_s3" {
+  type        = bool
+  description = "Push terraform output to S3"
+  default     = true
+}
+
+variable "existing_s3_bucket" {
+  type        = string
+  description = "Existing S3 Bucket"
+  default     = null
+}
+
+variable "objects_to_push" {
+  type        = map(any)
+  default     = {}
+  description = "A list of terraform objects that will be encoded to yaml and pushed to S3"
 }
