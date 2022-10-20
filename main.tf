5403d73f6af144eefb544adf9cbd80c364bc1513
Ciprian <cursu@axetrading.com>
2022-10-20 17:58:17 +0300

    update readme

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_public_access_block" "main" {
   block_public_acls       = true
   block_public_policy     = true
   restrict_public_buckets = true
-  ignore_public_acls = true
+  ignore_public_acls      = true
 }
 
 resource "aws_s3_bucket_versioning" "main" {
