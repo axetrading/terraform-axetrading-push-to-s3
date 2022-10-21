a89bf6ffeb621c04f522d5df1157934b08c0dc0f
Ciprian <cursu@axetrading.com>
2022-10-21 11:58:00 +0300

    fixing data

--- data.tf
+++ data.tf
@@ -41,10 +41,7 @@ data "aws_iam_policy_document" "cross_account_access" {
     }
 
     actions = [
-      "s3:Get*",
-      "s3:DeleteObject",
-      "s3:PutObject",
-      "s3:PutObjectAcl"
+      "s3:ListBucket"
     ]
 
     resources = [
